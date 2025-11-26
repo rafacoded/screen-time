0a5770c6232b6b2905e5bc724bb5c141991f7699
rafa <rafalintirado50@gmail.com>
2025-11-26 13:36:25 +0100

    Pantalla principal con slide menu y pestañas - Rafa

--- app/src/main/java/com/example/screentime/MainActivity.kt
+++ app/src/main/java/com/example/screentime/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.example.screentime
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.screentime.paginas.HomeActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val botonAcceso = findViewById<Button>(R.id.btnAcceder)
+
+        botonAcceso.setOnClickListener {
+            startActivity(Intent(this, HomeActivity::class.java))
         }
 
     }
--- app/src/main/java/com/example/screentime/paginas/HomeActivity.kt
+++ app/src/main/java/com/example/screentime/paginas/HomeActivity.kt
@@ -1,30 +1,67 @@
 package com.example.screentime.paginas
 
 import android.os.Bundle
+import android.view.Gravity
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.viewpager2.widget.ViewPager2
 import com.example.screentime.R
 import com.example.screentime.utils.MyPagerAdapter
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.navigation.NavigationView
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        val toolbar = findViewById<MaterialToolbar>(R.id.topAppBar)
+        setSupportActionBar(toolbar)
+
         val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
         val viewPager = findViewById<ViewPager2>(R.id.viewPager)
+
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
+        val navView = findViewById<NavigationView>(R.id.navigationView)
+
+
+        toolbar.setNavigationOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        navView.setNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_perfil -> {
+                    // Ir a activity: Perfil
+                }
+                R.id.nav_configuracion -> {
+                    // Ir a activity: Configuración
+                }
+                R.id.nav_sobre -> {
+                    // Ir a activity: Sobre Nosotros
+                }
+                R.id.nav_logout -> {
+                    // cerrar sesión
+                }
+            }
+            drawerLayout.closeDrawer(GravityCompat.START)
+            true
+        }
 
         val adapter = MyPagerAdapter(this)
         viewPager.adapter = adapter
 
         TabLayoutMediator(tabLayout, viewPager) { tab, position ->
             tab.text = when (position) {
-                0 -> "Reviews"
-                1 -> "Recordatorios"
-                else -> "Favoritas"
+                0 -> "Pendientes"
+                1 -> "Viendo"
+                2 -> "Vistas"
+                else -> ""
             }
         }.attach()
     }
