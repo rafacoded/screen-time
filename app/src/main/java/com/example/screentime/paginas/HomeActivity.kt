7c5342d9d41ffca4cf0c59e6441a4ccf069d1f24
JFuertesGarcia <jfuertesgarcia@safareyes.es>
2025-12-05 13:16:08 +0100

    Login funcional - Jaime

--- app/src/main/java/com/example/screentime/paginas/HomeActivity.kt
+++ app/src/main/java/com/example/screentime/paginas/HomeActivity.kt
@@ -1,10 +1,14 @@
 package com.example.screentime.paginas
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import com.bumptech.glide.Glide
 import com.example.screentime.R
 import com.example.screentime.fragments.BuscarFragment
 import com.example.screentime.fragments.CalendarioFragment
@@ -12,6 +16,7 @@ import com.example.screentime.fragments.HomeFragment
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
+import com.google.android.material.tabs.TabLayout
 
 
 class HomeActivity : AppCompatActivity() {
@@ -24,27 +29,23 @@ class HomeActivity : AppCompatActivity() {
         val toolbar = findViewById<MaterialToolbar>(R.id.topAppBar)
         setSupportActionBar(toolbar)
 
-//        val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
-//        val viewPager = findViewById<ViewPager2>(R.id.viewPager)
-
         val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
         val navView = findViewById<NavigationView>(R.id.navigationView)
 
         val bottomNavBar = findViewById<BottomNavigationView>(R.id.bottomNav)
-        // Corregido: Obtener los datos del Intent con las claves correctas
-//        val userId = intent.getIntExtra("userId", -1)
-//        val userNombre = intent.getStringExtra("userNombre")
-//        val userEmail = intent.getStringExtra("userEmail")
-//        val userDesc = intent.getStringExtra("userDesc")
-//        val userFoto = intent.getStringExtra("userFoto")
-//
-//        val headerView = navView.getHeaderView(0)
-//        val headerName = headerView.findViewById<TextView>(R.id.headerName)
-//        val headerImage = headerView.findViewById<ImageView>(R.id.headerImage)
-//
-//        // Asignar los valores a las vistas del header
-//        headerName.text = userNombre
-//        Glide.with(this).load(userFoto).into(headerImage)
+        val userId = intent.getIntExtra("userId", -1)
+        val userNombre = intent.getStringExtra("userNombre")
+        val userEmail = intent.getStringExtra("userEmail")
+        val userDesc = intent.getStringExtra("userDesc")
+        val userFoto = intent.getStringExtra("userFoto")
+
+        val headerView = navView.getHeaderView(0)
+        val headerName = headerView.findViewById<TextView>(R.id.headerName)
+        val headerImage = headerView.findViewById<ImageView>(R.id.headerImage)
+
+        // Asignar los valores a las vistas del header
+        headerName.text = userNombre
+        Glide.with(this).load(userFoto).into(headerImage)
 
         // DRAWER NAVIGATION
         toolbar.setNavigationOnClickListener {
